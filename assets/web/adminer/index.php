c089d6b76ce3b9a042e492d2ce697f348f02e52f
Odei Alba <odeialba@gmail.com>
2021-09-22 11:32:00 +0200

    All apache configurations prepared

---
+++ assets/web/adminer/index.php
@@ -0,0 +1,22 @@
+<?php
+function adminer_object() {
+    // required to run any plugin
+    include_once "./plugins/plugin.php";
+    
+    // autoloader
+    foreach (glob("plugins/*.php") as $filename) {
+        include_once "./$filename";
+    }
+    
+    $plugins = [
+        // specify enabled plugins here
+        new AdminerJsonPreview(),
+        new AdminerTablesFilter(),
+    ];
+    
+    return new AdminerPlugin($plugins);
+}
+
+// include original Adminer or Adminer Editor
+include "./adminer.php";
+?>
