bc4521424964b5f87f7a31aa94b8c0673bccbd1b
Odei Alba <odeialba@gmail.com>
2022-01-28 12:12:34 +0100

    Add oneclick-login to adminer

--- assets/web/adminer/index.php
+++ assets/web/adminer/index.php
@@ -1,4 +1,65 @@
 <?php
+/**
+ * Gets all servers
+ *
+ * @return array
+ */
+function get_login_servers(): array
+{
+    $username = 'moodle';
+    $password = 'm@0dl3ing';
+    $defaultdatabase = 'moodle';
+    $defaultlabel = 'Moodle';
+    return [
+        'db-pg' => array(
+            // Required parameters
+            'username'  => $username,
+            'pass'      => $password,
+            // Optional parameters
+            'driver'    => 'pgsql',     // if omitted, defaults to 'server'
+            'label'     => 'PosgreSQL',
+            'databases' => array(
+                'moodlewp' => 'Workplace',
+                'moodlelms' => 'LMS',
+                'phpunit' => 'PHPUnit',
+                'behat' => 'Behat'
+            )
+        ),
+        'db-mysql' => array(
+            // Required parameters
+            'username'  => $username,
+            'pass'      => $password,
+            // Optional parameters
+            'label'     => 'MySQL',
+            'databases' => array(
+                $defaultdatabase => $defaultlabel,
+            )
+        ),
+        'db-oracle' => array(
+            // Required parameters
+            'username'  => $username,
+            'pass'      => $password,
+            // Optional parameters
+            'driver'    => 'oracle',
+            'label'     => 'Oracle',
+            'databases' => array(
+                $defaultdatabase => $defaultlabel,
+            )
+        ),
+        'db-mssql' => array(
+            // Required parameters
+            'username'  => $username,
+            'pass'      => $password,
+            // Optional parameters
+            'driver'    => 'mssql',
+            'label'     => 'MicrosoftSQL',
+            'databases' => array(
+                $defaultdatabase => $defaultlabel,
+            )
+        ),
+    ];
+}
+
 function adminer_object() {
     // required to run any plugin
     include_once "./plugins/plugin.php";
@@ -15,6 +76,7 @@ function adminer_object() {
         new AdminerTableHeaderScroll(),
         new AdminerTablesFilter(),
         new AdminerSimpleMenu(true, false),
+        new OneClickLogin(get_login_servers()),
     ];
     
     return new AdminerPlugin($plugins);
