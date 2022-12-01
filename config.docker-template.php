b1d7f3464d4a96ee95cb6d5b0924be54129cc364
Odei Alba <odeialba@gmail.com>
2022-12-01 15:14:03 +0100

    Add short prefix on oracle db

--- config.docker-template.php
+++ config.docker-template.php
@@ -121,6 +121,12 @@ $CFG->behat_profiles = array(
 $CFG->behat_faildump_path = '/var/www/behatfaildumps';
 $CFG->behat_increasetimeout = getenv('MOODLE_DOCKER_TIMEOUT_FACTOR');
 
+if (getenv('MOODLE_DOCKER_DBHOST') === 'db-oracle') {
+    $CFG->prefix = 'm_';
+    $CFG->phpunit_prefix = 't_';
+    $CFG->behat_prefix = 'b_';
+}
+
 define('PHPUNIT_LONGTEST', true);
 
 if (getenv('MOODLE_DOCKER_APP')) {
